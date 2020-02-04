302b91ef5a7874c8aa8e3b46e4c59f0f306750a2
Jeff Goeders <jeff.goeders@gmail.com>
2020-02-03 17:04:49 -0700

    Add files via upload

--- lab1/main.c
+++ lab1/main.c
@@ -1,3 +1,21 @@
+
+// Print out "hello world" on both the console and the LCD screen.
+
+#include <stdio.h>
+#include "supportFiles/display.h"
 int main() {
-    return 0;
+	display_init();  // Must init all of the software and underlying hardware for LCD.
+	display_fillScreen(DISPLAY_BLACK);  // Blank the screen.
+	display_setCursor(0, 0);            // The upper left of the LCD screen.
+	display_setTextColor(DISPLAY_RED);  // Make the text red.
+	display_setTextSize(2);             // Make the text a little larger.
+	display_println("hello world (on the LCD)!");    // This actually prints the string to the LCD.
+	printf("hello world!\n\r");  // This prints on the console.
+}
+
+// This function must be defined but can be left empty for now.
+// You will use this function in a later lab.
+// It is called in the timer interrupt service routine (see interrupts.c in supportFiles).
+void isr_function() {
+    // Empty for now.
 }
