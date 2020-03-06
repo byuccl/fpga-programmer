228a93b19ccb3810d5ce15cefff7df329b86db85
Jeff Goeders <jeff.goeders@gmail.com>
2020-03-06 10:42:21 -0700

    lab7 files

---
+++ lab7/wamDisplay.c
@@ -0,0 +1,33 @@
+#include "supportFiles/display.h"
+#include "wamDisplay.h"
+#include "wamControl.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/********************** typedefs **********************/
+// This keeps track of all mole information.
+typedef struct {
+        wamDisplay_point_t origin;  // This is the origin of the hole for this mole.
+        // A mole is active if either of the tick counts are non-zero. The mole is dormant otherwise.
+        // During operation, non-zero tick counts are decremented at a regular rate by the control state machine.
+        // The mole remains in his hole until ticksUntilAwake decrements to zero and then he pops out.
+        // The mole remains popped out of his hole until ticksUntilDormant decrements to zero.
+        // Once ticksUntilDomant goes to zero, the mole hides in his hole and remains dormant until activated again.
+        wamDisplay_moleTickCount_t ticksUntilAwake;  // Mole will wake up (pop out of hole) when this goes from 1 -> 0.
+        wamDisplay_moleTickCount_t ticksUntilDormant; // Mole will go dormant (back in hole) this goes 1 -> 0.
+} wamDisplay_moleInfo_t;
+
+// This will contain pointers to all of the mole info records.
+// This will ultimately be treated as an array of pointers.
+static wamDisplay_moleInfo_t** wamDisplay_moleInfo;
+
+// Allocates the memory for wamDisplay_moleInfo_t records.
+// Computes the origin for each mole assuming a simple row-column layout:
+// 9 moles: 3 rows, 3 columns, 6 moles: 2 rows, 3 columns, 4 moles: 2 rows, 2 columns
+// Also inits the tick counts for awake and dormant.
+void wamDisplay_computeMoleInfo() {
+    // Setup all of the moles, creates and inits mole info records.
+    // Create the container array. It contains pointers to each of the mole-hole info records.
+    //    wamDisplay_moleInfo =  // Need to uncomment this line and initialize this variable via malloc().
+}
