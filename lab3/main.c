1ecc49e44f30d2720bb8cc530d2915e291941306
Jeff <jeff.goeders@Gmail.com>
2020-04-28 08:29:39 -0600

    Fix lab3 errors and warnings in main

--- lab3/main.c
+++ lab3/main.c
@@ -1,14 +1,19 @@
-#include "mylibs/buttons.h" // Modify this to reflect the location of your buttons.h
-#include "mylibs/intervalTimer.h" // Modify this to reflect the location of your intervalTimer.h
-#include "supportFiles/utils.h"
-#include "xil_io.h"
+#include <stdint.h>
 #include <stdio.h>
+
+#include "emulator.h"
+#include "xil_io.h"
+#include "xparameters.h"
+
+#include "my_libs/buttons.h" // Modify this to reflect the location of your buttons.h
+#include "my_libs/intervalTimer.h" // Modify this to reflect the location of your intervalTimer.h
+#include "supportFiles/utils.h"
 
 #define TCR0_OFFSET 0x08 // register offset for TCR0
 #define TCR1_OFFSET 0x18 // register offset for TCR1
 
 // Reads the timer1 registers based upon the offset.
-u32 readTimer1Register(uint32_t registerOffset) {
+uint32_t readTimer1Register(uint32_t registerOffset) {
   uint32_t address = XPAR_AXI_TIMER_0_BASEADDR +
                      registerOffset; // Add the offset to the base address.
   return Xil_In32(address);          // Read the register at that address.
@@ -31,35 +36,35 @@ void milestone1() {
   intervalTimer_reset(INTERVAL_TIMER_TIMER_0); // Reset timer 0.
   // Show that the timer is reset.
   // Check lower register.
-  printf("timer_0 TCR0 should be 0 at this point:%ld\n\r",
+  printf("timer_0 TCR0 should be 0 at this point:%d\n\r",
          readTimer1Register(TCR0_OFFSET));
   // Check upper register.
-  printf("timer_0 TCR1 should be 0 at this point:%ld\n\r",
+  printf("timer_0 TCR1 should be 0 at this point:%d\n\r",
          readTimer1Register(TCR1_OFFSET));
   intervalTimer_start(INTERVAL_TIMER_TIMER_0); // Start timer 0.
   // Show that the timer is running.
   printf("The following register values should be changing while reading "
          "them.\n\r");
   // Just checking multiple times to see if the timer is running.
-  printf("timer_0 TCR0 should be changing at this point:%ld\n\r",
+  printf("timer_0 TCR0 should be changing at this point:%d\n\r",
          readTimer1Register(TCR0_OFFSET));
-  printf("timer_0 TCR0 should be changing at this point:%ld\n\r",
+  printf("timer_0 TCR0 should be changing at this point:%d\n\r",
          readTimer1Register(TCR0_OFFSET));
-  printf("timer_0 TCR0 should be changing at this point:%ld\n\r",
+  printf("timer_0 TCR0 should be changing at this point:%d\n\r",
          readTimer1Register(TCR0_OFFSET));
-  printf("timer_0 TCR0 should be changing at this point:%ld\n\r",
+  printf("timer_0 TCR0 should be changing at this point:%d\n\r",
          readTimer1Register(TCR0_OFFSET));
-  printf("timer_0 TCR0 should be changing at this point:%ld\n\r",
+  printf("timer_0 TCR0 should be changing at this point:%d\n\r",
          readTimer1Register(TCR0_OFFSET));
   // Wait about 2 minutes so that you roll over to TCR1.
   // If you don't see a '1' or '2' in TCR1 after this long wait you probably
   // haven't programmed the timer correctly.
   waitALongTime();
   // Check lower register.
-  printf("timer_0 TCR0 value after wait:%lx\n\r",
+  printf("timer_0 TCR0 value after wait:%x\n\r",
          readTimer1Register(TCR0_OFFSET));
   // Check upper register.
-  printf("timer_0 TCR1 should have changed at this point:%ld\n\r",
+  printf("timer_0 TCR1 should have changed at this point:%d\n\r",
          readTimer1Register(TCR1_OFFSET));
 }
 
