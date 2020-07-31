099ecae5acbae6157e0157c9104d1205c41eb924
Jeff Goeders <jeff.goeders@gmail.com>
2020-07-31 14:25:46 -0600

    Fix lab5 magic numbers

--- lab5/testBoards.c
+++ lab5/testBoards.c
@@ -12,64 +12,70 @@ For questions, contact Brad Hutchings or Jeff Goeders, https://ece.byu.edu/
 #include "minimax.h"
 #include <stdio.h>
 
+#define TOP 0
+#define MID 1
+#define BOT 2
+#define LFT 0
+#define RGT 2
+
 // Test the next move code, given several boards.
 // You need to also create 10 boards of your own to test.
 int testBoards() {
   minimax_board_t board1; // Board 1 is the main example in the web-tutorial
                           // that I use on the web-site.
-  board1.squares[0][0] = MINIMAX_O_SQUARE;
-  board1.squares[0][1] = MINIMAX_EMPTY_SQUARE;
-  board1.squares[0][2] = MINIMAX_X_SQUARE;
-  board1.squares[1][0] = MINIMAX_X_SQUARE;
-  board1.squares[1][1] = MINIMAX_EMPTY_SQUARE;
-  board1.squares[1][2] = MINIMAX_EMPTY_SQUARE;
-  board1.squares[2][0] = MINIMAX_X_SQUARE;
-  board1.squares[2][1] = MINIMAX_O_SQUARE;
-  board1.squares[2][2] = MINIMAX_O_SQUARE;
+  board1.squares[TOP][LFT] = MINIMAX_O_SQUARE;
+  board1.squares[TOP][MID] = MINIMAX_EMPTY_SQUARE;
+  board1.squares[TOP][RGT] = MINIMAX_X_SQUARE;
+  board1.squares[MID][LFT] = MINIMAX_X_SQUARE;
+  board1.squares[MID][MID] = MINIMAX_EMPTY_SQUARE;
+  board1.squares[MID][RGT] = MINIMAX_EMPTY_SQUARE;
+  board1.squares[BOT][LFT] = MINIMAX_X_SQUARE;
+  board1.squares[BOT][MID] = MINIMAX_O_SQUARE;
+  board1.squares[BOT][RGT] = MINIMAX_O_SQUARE;
 
   minimax_board_t board2;
-  board2.squares[0][0] = MINIMAX_O_SQUARE;
-  board2.squares[0][1] = MINIMAX_EMPTY_SQUARE;
-  board2.squares[0][2] = MINIMAX_X_SQUARE;
-  board2.squares[1][0] = MINIMAX_EMPTY_SQUARE;
-  board2.squares[1][1] = MINIMAX_EMPTY_SQUARE;
-  board2.squares[1][2] = MINIMAX_EMPTY_SQUARE;
-  board2.squares[2][0] = MINIMAX_X_SQUARE;
-  board2.squares[2][1] = MINIMAX_EMPTY_SQUARE;
-  board2.squares[2][2] = MINIMAX_O_SQUARE;
+  board2.squares[TOP][LFT] = MINIMAX_O_SQUARE;
+  board2.squares[TOP][MID] = MINIMAX_EMPTY_SQUARE;
+  board2.squares[TOP][RGT] = MINIMAX_X_SQUARE;
+  board2.squares[MID][LFT] = MINIMAX_EMPTY_SQUARE;
+  board2.squares[MID][MID] = MINIMAX_EMPTY_SQUARE;
+  board2.squares[MID][RGT] = MINIMAX_EMPTY_SQUARE;
+  board2.squares[BOT][LFT] = MINIMAX_X_SQUARE;
+  board2.squares[BOT][MID] = MINIMAX_EMPTY_SQUARE;
+  board2.squares[BOT][RGT] = MINIMAX_O_SQUARE;
 
   minimax_board_t board3;
-  board3.squares[0][0] = MINIMAX_O_SQUARE;
-  board3.squares[0][1] = MINIMAX_EMPTY_SQUARE;
-  board3.squares[0][2] = MINIMAX_EMPTY_SQUARE;
-  board3.squares[1][0] = MINIMAX_O_SQUARE;
-  board3.squares[1][1] = MINIMAX_EMPTY_SQUARE;
-  board3.squares[1][2] = MINIMAX_EMPTY_SQUARE;
-  board3.squares[2][0] = MINIMAX_X_SQUARE;
-  board3.squares[2][1] = MINIMAX_EMPTY_SQUARE;
-  board3.squares[2][2] = MINIMAX_X_SQUARE;
+  board3.squares[TOP][LFT] = MINIMAX_O_SQUARE;
+  board3.squares[TOP][MID] = MINIMAX_EMPTY_SQUARE;
+  board3.squares[TOP][RGT] = MINIMAX_EMPTY_SQUARE;
+  board3.squares[MID][LFT] = MINIMAX_O_SQUARE;
+  board3.squares[MID][MID] = MINIMAX_EMPTY_SQUARE;
+  board3.squares[MID][RGT] = MINIMAX_EMPTY_SQUARE;
+  board3.squares[BOT][LFT] = MINIMAX_X_SQUARE;
+  board3.squares[BOT][MID] = MINIMAX_EMPTY_SQUARE;
+  board3.squares[BOT][RGT] = MINIMAX_X_SQUARE;
 
   minimax_board_t board4;
-  board4.squares[0][0] = MINIMAX_O_SQUARE;
-  board4.squares[0][1] = MINIMAX_EMPTY_SQUARE;
-  board4.squares[0][2] = MINIMAX_EMPTY_SQUARE;
-  board4.squares[1][0] = MINIMAX_EMPTY_SQUARE;
-  board4.squares[1][1] = MINIMAX_EMPTY_SQUARE;
-  board4.squares[1][2] = MINIMAX_EMPTY_SQUARE;
-  board4.squares[2][0] = MINIMAX_X_SQUARE;
-  board4.squares[2][1] = MINIMAX_EMPTY_SQUARE;
-  board4.squares[2][2] = MINIMAX_X_SQUARE;
+  board4.squares[TOP][LFT] = MINIMAX_O_SQUARE;
+  board4.squares[TOP][MID] = MINIMAX_EMPTY_SQUARE;
+  board4.squares[TOP][RGT] = MINIMAX_EMPTY_SQUARE;
+  board4.squares[MID][LFT] = MINIMAX_EMPTY_SQUARE;
+  board4.squares[MID][MID] = MINIMAX_EMPTY_SQUARE;
+  board4.squares[MID][RGT] = MINIMAX_EMPTY_SQUARE;
+  board4.squares[BOT][LFT] = MINIMAX_X_SQUARE;
+  board4.squares[BOT][MID] = MINIMAX_EMPTY_SQUARE;
+  board4.squares[BOT][RGT] = MINIMAX_X_SQUARE;
 
   minimax_board_t board5;
-  board5.squares[0][0] = MINIMAX_X_SQUARE;
-  board5.squares[0][1] = MINIMAX_X_SQUARE;
-  board5.squares[0][2] = MINIMAX_EMPTY_SQUARE;
-  board5.squares[1][0] = MINIMAX_EMPTY_SQUARE;
-  board5.squares[1][1] = MINIMAX_O_SQUARE;
-  board5.squares[1][2] = MINIMAX_EMPTY_SQUARE;
-  board5.squares[2][0] = MINIMAX_EMPTY_SQUARE;
-  board5.squares[2][1] = MINIMAX_EMPTY_SQUARE;
-  board5.squares[2][2] = MINIMAX_EMPTY_SQUARE;
+  board5.squares[TOP][LFT] = MINIMAX_X_SQUARE;
+  board5.squares[TOP][MID] = MINIMAX_X_SQUARE;
+  board5.squares[TOP][RGT] = MINIMAX_EMPTY_SQUARE;
+  board5.squares[MID][LFT] = MINIMAX_EMPTY_SQUARE;
+  board5.squares[MID][MID] = MINIMAX_O_SQUARE;
+  board5.squares[MID][RGT] = MINIMAX_EMPTY_SQUARE;
+  board5.squares[BOT][LFT] = MINIMAX_EMPTY_SQUARE;
+  board5.squares[BOT][MID] = MINIMAX_EMPTY_SQUARE;
+  board5.squares[BOT][RGT] = MINIMAX_EMPTY_SQUARE;
 
   uint8_t row, column;
 
