232e8b9e295af7c151b60d2db9d30f603fcbbe06
Jeff Goeders <jeff.goeders@gmail.com>
2020-08-08 08:22:54 -0700

    Fix lab4 flag method

--- lab4/main.c
+++ lab4/main.c
@@ -22,24 +22,21 @@ For questions, contact Brad Hutchings or Jeff Goeders, https://ece.byu.edu/
 #include "xparameters.h"
 
 #define MILESTONE_1 1
-#define MILESTONE_2_FLAG_METHOD 2
-#define MILESTONE_2_DIRECT_METHOD 3
+#define MILESTONE_2 2
 
 ////////////////////////////////////////////////////////////////////////////////
 // Uncomment one of the following lines to run Milestone 1 or 2      ///////////
 ////////////////////////////////////////////////////////////////////////////////
 // #define RUN_PROGRAM MILESTONE_1
-// #define RUN_PROGRAM MILESTONE_2_FLAG_METHOD
-// #define RUN_PROGRAM MILESTONE_2_DIRECT_METHOD
+// #define RUN_PROGRAM MILESTONE_2
 
-// If nothing is uncommented above, run milestone 2, flag method
+// If nothing is uncommented above, run milestone 2
 #ifndef RUN_PROGRAM
-#define RUN_PROGRAM MILESTONE_2_FLAG_METHOD
+#define RUN_PROGRAM MILESTONE_2
 #endif
 
-#define RUN_DISPLAY_TEST_MSG "Running clockDisplay_test()\n"
-#define ISR_INVOCATION_MESSAGE "Running the clock lab using isr_function()\n"
-#define FLAG_INVOCATION_MESSAGE "Running the clock lab using the flag method\n"
+#define RUN_DISPLAY_TEST_MSG "Running Milestone 1: clockDisplay_test()\n"
+#define RUN_MILESTONE_2_MSG "Running Milestone 2: full clock lab\n"
 
 // The formula for computing the load value is based upon the formula
 // from 4.1.1
@@ -64,44 +61,10 @@ int main() {
   printf(RUN_DISPLAY_TEST_MSG);
   clockDisplay_runTest();
 
-#elif (RUN_PROGRAM == MILESTONE_2_DIRECT_METHOD)
-  printf(ISR_INVOCATION_MESSAGE);
-  // Initialize the GPIO LED driver and print out an error message if it fails
-  // (argument = true). You need to init the LEDs so that LD4 can function as a
-  // heartbeat.
-  leds_init(true);
-  // Init all interrupts (but does not enable the interrupts at the devices).
-  // Prints an error message if an internal failure occurs because the argument
-  // = true.
-  interrupts_initAll(true);
-  interrupts_setPrivateTimerLoadValue(TIMER_LOAD_VALUE);
-  printf("timer load value:%d\n\r", (int32_t)TIMER_LOAD_VALUE);
-  interrupts_enableTimerGlobalInts();
-  // Initialization of the clock display is not time-dependent, do it outside of
-  // the state machine.
-  clockDisplay_init();
-  clockControl_init();
-  // Start the private ARM timer running.
-  interrupts_startArmPrivateTimer();
-  // Enable interrupts at the ARM.
-  interrupts_enableArmInts();
-  // Disabled this for the moment to reduce CPU load.
-  // The while-loop just waits until the total number of timer ticks have
-  // occurred before proceeding.
-  while (1) {
-    utils_sleep();
-    if (isr_functionCallCount == MAX_INTERRUPT_COUNT)
-      break;
-  }
-  // All done, now disable interrupts and print out the interrupt counts.
-  interrupts_disableArmInts();
-  printf("isr invocation count: %d\n\r", interrupts_isrInvocationCount());
-  printf("internal interrupt count: %d\n\r", isr_functionCallCount);
-
-#elif (RUN_PROGRAM == MILESTONE_2_FLAG_METHOD)
+#elif (RUN_PROGRAM == MILESTONE_2)
   // This main() uses the flag method to invoke clockControl_tick().
 
-  printf(FLAG_INVOCATION_MESSAGE);
+  printf(RUN_MILESTONE_2_MSG);
   // Initialize the GPIO LED driver and print out an error message if it fails
   // (argument = true). You need to init the LEDs so that LD4 can function as
   // a heartbeat.
@@ -141,13 +104,7 @@ int main() {
   return 0;
 }
 
-void isr_function() {
-#if (RUN_PROGRAM == MILESTONE_2_DIRECT_METHOD)
-  // The clockControl_tick() function is now called directly by the timer
-  // interrupt service routine.
-  clockControl_tick();     // tick gets called directly by the timer interrupt
-                           // service routine.
-  isr_functionCallCount++; // Keep track of how many times you are called to
-// ensure that you don't miss interrupts.
-#endif
-}
+// Keep this empty
+// The 'interrupts_isrFlagGlobal' flag will be automatically set on an interrupt
+// behind the scenes.  We don't need to set it here.
+void isr_function() {}
