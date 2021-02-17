13d1a83c87ceae864febbe1d1f2cda717b00e0e0
Jeff Goeders <jeff.goeders@gmail.com>
2021-02-17 15:17:33 -0700

    Init

---
+++ fpga_programmer.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python3
+
+import argparse
+
+
+BOARDS = 
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("board", choices=)
+    parser.add_argument("bit", help="FPGA bitstream (.bit) path")
+    args = parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
